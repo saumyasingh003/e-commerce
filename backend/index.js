b4bc90816f88b112f82fd9199d58a1f65dbb087d
saumyasingh003 <saumyasingh98982@gmail.com>
2025-09-08 00:25:59 +0530

    updates cors

--- backend/index.js
+++ backend/index.js
@@ -9,12 +9,13 @@ import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
 import paymentRoutes from "./routes/payment.route.js";
 import analyticsRoutes from "./routes/analytics.route.js";
-
+import cors from "cors";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
